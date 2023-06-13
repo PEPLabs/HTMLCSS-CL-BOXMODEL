0edf3fe20d9a62c344c12c247d9601299478a32f
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2023-06-13 17:14:21 -0400

    add webdrivermanager for automated web drive mgmt and refactor selenium tests

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,43 +1,82 @@
-import static org.junit.Assert.assertEquals;
 
 import java.io.File;
-import java.util.List;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class SeleniumTest {
 
     private WebDriver driver;
 
-    @Before
-    public void setUp() {
-        // Set up ChromeDriver path
-        System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
-
-        // Create a new ChromeDriver instance
+    @BeforeEach
+    void setup() {
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         File file = new File("CSSBoxModel.html");
-        // Open the HTML file
-        driver.get(file.getAbsolutePath());
+        driver.get("file:///" + file.getAbsolutePath());
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+//    @Before
+//    public void setUp() {
+//        // Set up ChromeDriver path
+//        System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+//
+//        WebDriverManager.chromedriver().setup();
+//
+//        // Create a new ChromeDriver instance
+//        driver = new ChromeDriver();
+//        File file = new File("CSSBoxModel.html");
+//        // Open the HTML file
+//        driver.get(file.getAbsolutePath());
+//    }
+
+    @Test
+    public void testAllPizzaPadding() {
+        WebElement pizza = driver.findElement(By.className("pizza"));
+        assertEquals("20px", pizza.getCssValue("padding"));
     }
 
     @Test
-    public void testPizza1Padding() {
+    public void testPizza1BackgroundColor() {
         WebElement pizza1 = driver.findElement(By.id("pizza1"));
-        assertEquals("20px", pizza1.getCssValue("padding"));
+        assertEquals("rgba(255, 255, 0, 1)", pizza1.getCssValue("background-color")); // Expected value for yellow
     }
 
-    @After
-    public void tearDown() {
-        // Close the browser
-        driver.quit();
+    @Test
+    public void testPizza2Color() {
+        WebElement pizza2 = driver.findElement(By.id("pizza2"));
+        assertEquals("rgba(255, 0, 0, 1)", pizza2.getCssValue("color")); // Expected value for red
     }
+
+    @Test
+    public void testPizza2Border() {
+        WebElement pizza2 = driver.findElement(By.id("pizza2"));
+        assertEquals("5px solid rgb(139, 69, 19)", pizza2.getCssValue("border")); // Expected value for 5px solid saddlebrown
+    }
+
+    @Test
+    public void testPizza3Border() {
+        WebElement pizza3 = driver.findElement(By.id("pizza3"));
+        assertEquals("10px solid rgb(0, 0, 0)", pizza3.getCssValue("border")); // Expected value for black solid 10px
+    }
+
+//    @After
+//    public void tearDown() {
+//        // Close the browser
+//        driver.quit();
+//    }
 }
