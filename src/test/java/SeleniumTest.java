c1cc26d1f0f55cb674e69cbac08aa9f68b2a1e53
LiquidPlummer <kyle.plummer@revature.com>
2023-07-20 12:54:24 -0400

    fixing testing suite

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,62 +1,72 @@
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class SeleniumTest {
 
-    private WebDriver driver;
+    private WebDriver webDriver;
 
-    @BeforeEach
-    void setup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    @Before
+    public void setUp() {
+        // Set up ChromeDriver path
+        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
+
+        // Get file
         File file = new File("CSSBoxModel.html");
-        driver.get("file:///" + file.getAbsolutePath());
+        String path = "file://" + file.getAbsolutePath();
+
+        // Create a new ChromeDriver instance
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        webDriver = new ChromeDriver(options);
+
+        // Open the HTML file
+        webDriver.get(path);
     }
 
-    @AfterEach
+    @After
     void teardown() {
-        driver.quit();
+        webDriver.quit();
     }
 
     @Test
     public void testAllPizzaPadding() {
-        WebElement pizza = driver.findElement(By.className("pizza"));
+        WebElement pizza = webDriver.findElement(By.className("pizza"));
         assertEquals("20px", pizza.getCssValue("padding"));
     }
 
     @Test
     public void testAllPizzaMargin() {
-        WebElement pizza = driver.findElement(By.className("pizza"));
+        WebElement pizza = webDriver.findElement(By.className("pizza"));
         assertEquals("20px", pizza.getCssValue("margin"));
     }
 
     @Test
     public void testPizza1BackgroundColor() {
-        WebElement pizza1 = driver.findElement(By.id("pizza1"));
+        WebElement pizza1 = webDriver.findElement(By.id("pizza1"));
         assertEquals("rgba(255, 255, 0, 1)", pizza1.getCssValue("background-color")); // Expected value for yellow
     }
 
     @Test
     public void testPizza2Color() {
-        WebElement pizza2 = driver.findElement(By.id("pizza2"));
+        WebElement pizza2 = webDriver.findElement(By.id("pizza2"));
         assertEquals("rgba(255, 0, 0, 1)", pizza2.getCssValue("color")); // Expected value for red
     }
 
     @Test
     public void testPizza2Border() {
-        WebElement pizza2 = driver.findElement(By.id("pizza2"));
+        WebElement pizza2 = webDriver.findElement(By.id("pizza2"));
         assertEquals("solid", pizza2.getCssValue("border-style"));
         assertEquals("rgb(139, 69, 19)", pizza2.getCssValue("border-color"));
         // For some reason, possible due to inconsistencies between browsers, the border-width is sometimes
@@ -68,7 +78,7 @@ public class SeleniumTest {
 
     @Test
     public void testPizza3Border() {
-        WebElement pizza3 = driver.findElement(By.id("pizza3"));
+        WebElement pizza3 = webDriver.findElement(By.id("pizza3"));
         assertEquals("10px solid rgb(0, 0, 0)", pizza3.getCssValue("border")); // Expected value for black solid 10px
     }
 
